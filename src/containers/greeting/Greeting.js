111186e97df2ec0b0286d631816f4222bf5340cf
Luke Lawton <llawton25@student.dimanregional.org>
2025-03-23 20:41:12 -0400

    epdated code

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,8 +42,8 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("../../assets/images/resume.pdf")}
-                    download="resume.pdf"
+                    href={require("./resume.pdf")}
+                    download="Resume.pdf"
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
