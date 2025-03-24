fe63873cdc7f2dff4229f155534b5987d42ab40d
Luke Lawton <llawton25@student.dimanregional.org>
2025-03-23 20:46:02 -0400

    okay i need to try this

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -43,7 +43,7 @@ export default function Greeting() {
                 {greeting.resumeLink && (
                   <a
                     href={require("./resume.png")}
-                    download="Resume.pdf";
+                    download="Resume.pdf"
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
